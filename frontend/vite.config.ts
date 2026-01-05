c233886a530894f97c67fe1f8f5c43b8f3d758d3
daimatz <dai@daimatz.net>
2026-01-06 02:14:12 +0900

    split markdown libraries

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,26 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (
+            id.includes("react-markdown") ||
+            id.includes("remark-gfm") ||
+            id.includes("remark-cjk-friendly") ||
+            id.includes("unified") ||
+            id.includes("remark-") ||
+            id.includes("micromark") ||
+            id.includes("mdast") ||
+            id.includes("hast")
+          ) {
+            return "markdown";
+          }
+        },
+      },
+    },
+  },
   server: {
     port: 5173,
     allowedHosts: ['0.0.0.0', 'disk-nan.exe.xyz'],
